51ce9ee2af647d54c73ebc27c14bac37834b2542
shinosock <you@example.com>
2023-07-10 20:43:39 +0800

    电压模拟温度进行温控变频

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -40,16 +40,21 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
 uint8_t spwm[21000];
-uint32_t freq = 50;
-double pointNum = 399;
-uint16_t Counter_sine1 = 0;	
-uint16_t Counter_sine2 = 133;
-uint16_t Counter_sine3 = 266;
+uint32_t freq = 5;
+double pointNum;
+uint32_t Counter_sine1;	
+uint32_t Counter_sine2;
+uint32_t Counter_sine3 ;
+uint32_t ADC_Value;
+uint32_t Last_ADC_Value1 = 0 ;
+uint32_t Last_ADC_Valuebuf;
 
 /* USER CODE END PV */
 
@@ -58,6 +63,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 void changeSpwm(void);
 /* USER CODE END PFP */
@@ -97,6 +103,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   MX_TIM2_Init();
+  MX_ADC1_Init(); 
   /* USER CODE BEGIN 2 */
   changeSpwm();
   HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_1);
@@ -115,7 +122,23 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    HAL_ADC_Start(&hadc1);     //启动ADC转换
+    HAL_ADC_PollForConversion(&hadc1, 50);   //等待转换完成，50为最大等待时间，单位为ms 
+    for(uint8_t i = 10;i>1;i--)
+		{
+			ADC_Value = HAL_ADC_GetValue(&hadc1);	
+			Last_ADC_Valuebuf = ADC_Value + Last_ADC_Valuebuf;
+		}		
+		Last_ADC_Valuebuf = Last_ADC_Valuebuf/10;
+    if (Last_ADC_Valuebuf - Last_ADC_Value1 > 200)
+    {
+      Last_ADC_Value1 = Last_ADC_Valuebuf;
+      freq = 5 + (Last_ADC_Value1*3.3f/4096)*80;
+      changeSpwm();
+    }
+    /* freq = 5 + (Last_ADC_Value1*3.3f/4096)*30;
+    changeSpwm();
+    HAL_Delay(1000); */
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -168,6 +191,58 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
   * @brief TIM1 Initialization Function
   * @param None
   * @retval None
@@ -188,7 +263,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 3;
+  htim1.Init.Prescaler = 5;
   htim1.Init.CounterMode = TIM_COUNTERMODE_CENTERALIGNED1;
   htim1.Init.Period = 199;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -269,7 +344,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 3;
+  htim2.Init.Prescaler = 5;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 199;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -320,10 +395,8 @@ void changeSpwm(void)
   const double pi = acos(-1.0);
   double x,r;
   double s;
-  pointNum = 105000 / freq;
-  Counter_sine1 = 0;
-  Counter_sine2 = pointNum/3;
-  Counter_sine3 = pointNum*2/3;
+  pointNum = 70000 / freq;
+  
 
   if (freq > 0)
   {
@@ -336,6 +409,9 @@ void changeSpwm(void)
       spwm[i] = s;
     } 
   }
+  Counter_sine1 = 0;
+  Counter_sine2 = pointNum*0.33333333333;
+  Counter_sine3 = pointNum*0.66666666666;
   
 }
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
